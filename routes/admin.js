04a1b29153f772db08268e3a88bec9e5d9552be0
kumaar <kumaar@DESKTOP-UL0FUTT.localdomain>
2022-12-12 22:30:04 +0530

    final update

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@ const GalleryModel = require("../models/gallery");
 route.get("/add-category/:categoryName", async (req, res, next) => {
     try {
         const categoryName = req.params.categoryName;
+
         if (!categoryName) {
             res.status(400).send("Bad Request");
         }
@@ -22,22 +23,27 @@ route.get("/add-category/:categoryName", async (req, res, next) => {
 });
 
 route.post("/add-image", async (req, res, next) => {
-    const name = req.body.name;
-    const category = req.body.category;
-    const imageUrl = req.body.imageUrl;
+    try {
+        const name = req.body.name;
+        const category = req.body.category;
+        const imageUrl = req.body.imageUrl;
 
-    if (!name || !category.length || !imageUrl) {
-        res.status(400).send("Bad Request");
+        if (!name || !category.length || !imageUrl) {
+            res.status(400).send("Bad Request");
+        }
+
+        const newGalleryData = {
+            name: name,
+            category: category,
+            imageUrl: imageUrl,
+        };
+
+        await GalleryModel.create(newGalleryData);
+        res.send("Image added successfully!");
+    } catch (error) {
+        console.log(error);
+        next(error);
     }
-
-    const newGalleryData = {
-        name: name,
-        category: category,
-        imageUrl: imageUrl,
-    };
-
-    await GalleryModel.create(newGalleryData);
-    res.send("Image added successfully!");
 });
 
 module.exports = route;
